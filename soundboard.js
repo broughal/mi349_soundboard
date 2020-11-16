c0837bbef558b36793d7ef301af93ecb211f829c
Alec <70782022+broughal@users.noreply.github.com>
2020-11-15 23:16:25 -0500

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -1,19 +1,6 @@
-
-var one = document.getElementById("buttonOne"),("buttonTwo"),("buttonThree");
+var clip = document.getElementById("button");
                                    
     function playAudio() {                                 
-        one.play();
-    }
-
-var two = document.getElementById("buttonTwo");
-                                   
-    function playAudio() {                                 
-        two.play();
-    }
-
-var three = document.getElementById("buttonThree");
-                                   
-    function playAudio() {                                 
-        three.play();
+        clip.play();
     }
 
